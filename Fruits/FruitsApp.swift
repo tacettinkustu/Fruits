00a6d814ad26617882e666935b5b06882069d105
Tacettin Kustu <tacettinkustu@gmail.com>
2023-05-29 21:49:54 +0300

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Tacettin Küstü on 29.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
