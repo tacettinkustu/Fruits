828e96633cf6661cc116f838c824eb265a4c479b
Tacettin Kustu <tacettinkustu@gmail.com>
2023-06-06 23:47:58 +0300

    Set first fruit card

--- Fruits/View/FruitCardView.swift
+++ Fruits/View/FruitCardView.swift
@@ -9,16 +9,21 @@ import SwiftUI
 
 struct FruitCardView: View {
     var body: some View {
-        VStack(spacing: 20) {
-            Image("blueberry")
-               .resizable()
-               .scaledToFit()
-               .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
-               // .scaleEffect(isAnimating ? 1.0 : 0.6)
-            
-            
-            Text("Blueberry")
-        } //: VStack
+        ZStack {
+            VStack(spacing: 20) {
+                Image("blueberry")
+                   .resizable()
+                   .scaledToFit()
+                   .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+                   // .scaleEffect(isAnimating ? 1.0 : 0.6)
+                               
+                Text("Blueberry")
+            } //: VStack
+        } //: ZStack
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
+        .padding(.horizontal, 20)
     }
 }
 
