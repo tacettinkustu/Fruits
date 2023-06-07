8d266c8513156faab276d859182031388c163f76
Tacettin Kustu <tacettinkustu@gmail.com>
2023-06-07 16:48:28 +0300

    Implement newly created button component into card_view

--- Fruits/View/FruitCardView.swift
+++ Fruits/View/FruitCardView.swift
@@ -28,6 +28,8 @@ struct FruitCardView: View {
                     .multilineTextAlignment(.center)
                     .padding(.horizontal, 16)
                     .frame(maxWidth: 480)
+                
+                StartButtonView()
             } //: VStack
         } //: ZStack
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
