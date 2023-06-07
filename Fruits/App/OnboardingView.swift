1b8204580bd564708166ba6288881825b35a4386
Tacettin Kustu <tacettinkustu@gmail.com>
2023-06-07 17:03:48 +0300

    Create OnboardingView component and use it

---
+++ Fruits/App/OnboardingView.swift
@@ -0,0 +1,28 @@
+//
+//  OnboardingView.swift
+//  Fruits
+//
+//  Created by Tacettin Küstü on 7.06.2023.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+   
+  var body: some View {
+    TabView {
+      ForEach(0..<5) { item in
+        FruitCardView()
+      } //: Loop
+    } //: Tab
+    .tabViewStyle(PageTabViewStyle())
+    .padding(.vertical, 20)
+  }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+  static var previews: some View {
+    OnboardingView()
+      .previewDevice("iPhone 14")
+  }
+}
