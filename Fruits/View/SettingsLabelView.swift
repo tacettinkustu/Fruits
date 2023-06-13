0bf57b0fc0f7c780463328d29e917dfa910c92b0
Tacettin Kustu <tacettinkustu@gmail.com>
2023-06-13 17:17:15 +0300

    Create settingsLabelView component to use it inside of SettingsView

---
+++ Fruits/View/SettingsLabelView.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsLabelView.swift
+//  Fruits
+//
+//  Created by Tacettin Küstü on 13.06.2023.
+//
+
+import SwiftUI
+
+struct SettingsLabelView: View {
+  
+  var labelText: String
+  var labelImage: String
+
+  var body: some View {
+    HStack {
+      Text(labelText.uppercased()).fontWeight(.bold)
+      Spacer()
+      Image(systemName: labelImage)
+    }
+  }
+}
+
+struct SettingsLabelView_Previews: PreviewProvider {
+  static var previews: some View {
+    SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")
+      .previewLayout(.sizeThatFits)
+      .padding()
+  }
+}
