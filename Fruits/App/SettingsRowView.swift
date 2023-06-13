73648d3bb2aa7e61c028b57919d7f19f57013c96
Tacettin Kustu <tacettinkustu@gmail.com>
2023-06-13 17:22:26 +0300

    Create SettingsRowView component and use it inside of SettingsView

---
+++ Fruits/App/SettingsRowView.swift
@@ -0,0 +1,50 @@
+//
+//  SettingsRowView.swift
+//  Fruits
+//
+//  Created by Tacettin Küstü on 13.06.2023.
+//
+
+import SwiftUI
+
+struct SettingsRowView: View {
+  
+  var name: String
+  var content: String? = nil
+  var linkLabel: String? = nil
+  var linkDestination: String? = nil
+
+  var body: some View {
+    VStack {
+      Divider().padding(.vertical, 4)
+      
+      HStack {
+        Text(name).foregroundColor(Color.gray)
+        Spacer()
+        if (content != nil) {
+          Text(content!)
+        } else if (linkLabel != nil && linkDestination != nil) {
+          Link(linkLabel!, destination: URL(string: "https://\(linkDestination!)")!)
+          Image(systemName: "arrow.up.right.square").foregroundColor(.pink)
+        }
+        else {
+          EmptyView()
+        }
+      }
+    }
+  }
+}
+
+struct SettingsRowView_Previews: PreviewProvider {
+  static var previews: some View {
+    Group {
+      SettingsRowView(name: "Developer", content: "John / Jane")
+        .previewLayout(.fixed(width: 375, height: 60))
+        .padding()
+      SettingsRowView(name: "Website", linkLabel: "SwiftUI Masterclass", linkDestination: "swiftuimasterclass.com")
+        .preferredColorScheme(.dark)
+        .previewLayout(.fixed(width: 375, height: 60))
+        .padding()
+    }
+  }
+}
